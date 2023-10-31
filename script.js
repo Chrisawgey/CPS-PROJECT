765719d488a9833089cf4d0607d5a33539f26bd0
Chrisawgey <124189207+Chrisawgey@users.noreply.github.com>
2023-10-31 09:15:09 -0400

    create chart

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Load the Google Charts library
 google.charts.load('current', { packages: ['corechart', 'table', 'geochart'] });
 
+
 // Variables to track whether a CSV file is loaded and the chart is displayed
 let isCSVLoaded = false;
 let isChartDisplayed = false;
@@ -9,14 +10,28 @@ let isChartDisplayed = false;
 let selectedChartType = null;
 
 // Store the loaded CSV data
-let loadedCSVData = null;
+let loadedCSVArray = [];
 
-// Function to clear the chart
+// Separate arrays for different chart types
+let barChartData = [];
+let lineChartData = [];
+let pieChartData = [];
+let mapChartData = [];
+
+// Function to clear the graph display area
 function clearChart() {
-    const googleTable = document.querySelector('.google-table');
-    googleTable.innerHTML = '';
+    const graphDisplay = document.querySelector('.graph-display');
+    graphDisplay.innerHTML = '';
     isChartDisplayed = false;
 }
+
+// Function to display the chart in the "graph-display" area
+function displayChart(chart) {
+    const graphDisplay = document.querySelector('.graph-display');
+    graphDisplay.appendChild(chart);
+    isChartDisplayed = true;
+}
+
 
 function clearViewErrorMessage() {
     const messageArea = document.getElementById('graph-display');
@@ -39,20 +54,28 @@ function displayCSVData(csvData) {
     const rows = csvData.split('\n');
     const headers = rows[0].split(',');
     const data = rows.slice(1);
+    loadedCSVArray = data.map(row => row.split(','));
+
+    if (selectedChartType) {
+        // Call the addDataToChartArray function with the appropriate chart type
+        addDataToChartArray(selectedChartType, loadedCSVArray);
+    }
+
+    // Define the indices of the columns you want to display
+    const columnIndices = [0, 1, 2, 3, 4, 5, 6, 7, 8]; // Adjust these indices as needed
+
     const dataTable = new google.visualization.DataTable();
-    const visibleColumns = [];
     headers.forEach((header, index) => {
-        const columnHasData = data.some(row => row.split(',')[index].trim() !== '');
-        if (columnHasData) {
+        if (columnIndices.includes(index)) {
             dataTable.addColumn('string', header);
-            visibleColumns.push(index);
         }
     });
     data.forEach((row) => {
         const rowData = row.split(',');
-        const filteredRowData = visibleColumns.map(index => rowData[index]);
+        const filteredRowData = columnIndices.map(index => rowData[index]);
         dataTable.addRow(filteredRowData);
     });
+
     const chart = new google.visualization.Table(document.querySelector('.google-table'));
     const maxWidth = 600;
     const maxHeight = 400;
@@ -62,53 +85,82 @@ function displayCSVData(csvData) {
     document.getElementById('message-area').textContent = `Number of Records: ${data.length}`;
 }
 
-function createBarChart(dataType) {
-    if (loadedCSVData) {
-        const data = new google.visualization.DataTable();
-        data.addColumn('string', 'X-Axis');
-        data.addColumn('number', 'Y-Axis');
-        // Populate data based on your CSV data
-        const options = {
-            title: 'Bar Chart Title',
-        };
-        const chart = new google.visualization.BarChart(document.getElementById('graph-display'));
-        chart.draw(data, options);
+
+function addDataToChartArray(chartType, data) {
+    const filteredData = data.map(row => [
+        row[0], // id
+        row[1], // state
+        parseFloat(row[2]), // total cases
+        parseFloat(row[3]), // new cases
+        parseFloat(row[4]), // total deaths
+        parseFloat(row[5]), // new deaths
+        parseFloat(row[6]), // total tests
+        parseFloat(row[7]), // population
+        row[8] // date
+    ]);
+
+    if (chartType === 'bar') {
+        barChartData = filteredData.map(row => [row[1], row[2]]);
+    } else if (chartType === 'line') {
+        lineChartData = filteredData.map(row => [row[1], row[2]]);
+    } else if (chartType === 'pie') {
+        pieChartData = filteredData.map(row => ({ label: row[1], value: row[2] }));
+    } else if (chartType === 'map') {
+        mapChartData = filteredData.map(row => ({
+            region: row[1],  // The region name (e.g., state)
+            value: row[2],   // The value associated with the region
+            // Add additional properties if needed
+        }));
+    }
+}
+const barChartDiv = document.getElementById('bar-chart');
+const lineChartDiv = document.getElementById('line-chart');
+const pieChartDiv = document.getElementById('pie-chart');
+const mapChartDiv = document.getElementById('map-chart');
+
+function createBarChart() {
+    if (barChartData.length > 0) {
+        // Create and display the bar chart
+        // ...
+
+        // Instructions: You need the HTML <div id='barchart'> </div> to display the chart.
+        // Ensure you have a <div> element with id 'barchart' in your HTML.
     }
 }
 
-function createLineChart(dataType) {
-    if (loadedCSVData) {
-        const data = new google.visualization.DataTable();
-        data.addColumn('string', 'X-Axis');
-        data.addColumn('number', 'Y-Axis');
-        // Populate data based on your CSV data
-        const options = {
-            title: 'Line Chart Title',
-        };
-        const chart = new google.visualization.LineChart(document.getElementById('graph-display'));
-        chart.draw(data, options);
+function createLineChart() {
+    if (lineChartData.length > 0) {
+        // Create and display the line chart
+        // ...
+
+        // Instructions: You need the HTML <div id='linechart'> </div> to display the chart.
+        // Ensure you have a <div> element with id 'linechart' in your HTML.
     }
 }
 
-function createPieChart(dataType, covidData) {
-    if (covidData) {
-        const data = new google.visualization.DataTable();
-        data.addColumn('string', 'Label');
-        data.addColumn('number', 'Value');
+function createPieChart() {
+    if (pieChartData.length > 0) {
+        // Create and display the pie chart
+        // ...
 
-        // Populate data based on the COVID-19 data
-        covidData.forEach(entry => {
-            data.addRow([entry.label, entry.value]);
-        });
-
-        const options = {
-            title: 'COVID-19 Distribution in New Jersey',
-        };
-
-        const chart = new google.visualization.PieChart(document.getElementById('graph-display'));
-        chart.draw(data, options);
+        // Instructions: You need the HTML <div id='piechart'> </div> to display the chart.
+        // Ensure you have a <div> element with id 'piechart' in your HTML.
     }
 }
+
+function createMapChart() {
+    if (mapChartData.length > 0) {
+        // Create and display the map chart
+        // ...
+
+        // Instructions: You need the HTML <div id='mapchart'> </div> to display the chart.
+        // Ensure you have a <div> element with id 'mapchart' in your HTML.
+    }
+}
+
+
+
+// You can add similar functions for creating other chart types, like mapChartData.
 
 document.getElementById('load-csv').addEventListener('click', function () {
     if (!isCSVLoaded) {
@@ -125,8 +177,26 @@ document.querySelectorAll('input[type="radio"]').forEach(radio => {
             return;
         }
         selectedChartType = this.value;
+
+        // Clear any existing chart if one is displayed
+        clearChart();
+
+        // Call the addDataToChartArray function with the appropriate chart type
+        addDataToChartArray(selectedChartType, loadedCSVArray);
+
+        // Depending on the selected chart type, create and display the chart.
+        if (selectedChartType === 'bar') {
+            createBarChart();
+        } else if (selectedChartType === 'line') {
+            createLineChart();
+        } else if (selectedChartType === 'pie') {
+            createPieChart();
+        } else if (selectedChartType === 'map') {
+            createMapChart();
+        }
     });
 });
+
 
 // Event listener for sub-menu items in the view menu
 const viewSubMenuItems = document.querySelectorAll('.menu li:has(ul) li a');
@@ -141,32 +211,22 @@ viewSubMenuItems.forEach(item => {
         const submenuItem = this.textContent.toLowerCase();
 
         if (selectedChartType === 'bar' || selectedChartType === 'line') {
-            if (
-                (submenuItem === 'avgwages' && (selectedChartType === 'bar' || selectedChartType === 'line')) ||
-                (submenuItem === 'estimatedpopulation' && (selectedChartType === 'bar' || selectedChartType === 'line')) ||
-                (submenuItem === 'state' && (selectedChartType === 'bar' || selectedChartType === 'pie'))
-            ) {
+            if (submenuItem === 'graph') {
                 if (selectedChartType === 'bar') {
-                    createBarChart(submenuItem);
+                    createBarChart();
                 } else if (selectedChartType === 'line') {
-                    createLineChart(submenuItem);
-                } else if (selectedChartType === 'pie') {
-                    createPieChart(submenuItem);
+                    createLineChart();
                 }
-                // Display "Success" when it's applicable
-                document.getElementById('graph-display').textContent = 'Success';
+                // Display the chart in the graph-display area
+                displayChart();
             } else {
-                // Display "Not Applicable" when it's not applicable
                 document.getElementById('graph-display').textContent = 'Not Applicable';
             }
         } else {
-            // Display "Not Applicable" if the selectedChartType is neither "bar" nor "line"
             document.getElementById('graph-display').textContent = 'Not Applicable';
         }
     });
 });
-
-
 
 
 document.getElementById('file-input').addEventListener('change', function (event) {
@@ -184,7 +244,6 @@ document.getElementById('file-input').addEventListener('change', function (event
                 const csvData = e.target.result;
                 displayCSVData(csvData);
                 isCSVLoaded = true;
-                loadedCSVData = csvData;
             };
             reader.readAsText(file);
         } else {
@@ -213,7 +272,7 @@ document.querySelector('.menu li ul li a[href="#Client"]').addEventListener('cli
     const javaEnabled = `Java Enabled: ${navigator.javaEnabled() ? 'Yes' : 'No'}\n`;
     const clientContent = `${browserInfo}${cookieEnabled}${javaEnabled}`;
     displayPopup(clientContent);
-});
+})
 
 function displayUserInfo() {
     const userInfo = {
